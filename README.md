d93f08c474f049eeb2224913b54c97ee61215e32
Kwynn <buessresume@gmail.com>
2020-07-31 21:34:16 -0400

    array in and out

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # php-date-formats
 PHP date() function formats such as r = Thu, 21 Dec 2000 16:01:07 +0200
+
+
+
+git remote set-url origin git@github.com:kwynncom/php-date-formats.git
--- server.php
+++ server.php
@@ -6,8 +6,14 @@ doit();
 
 function doit() {
     kwas(isset($_REQUEST['format']), 'no format sent');
-    $f =       $_REQUEST['format'];
-    kwas(!preg_match('/^[^A-Za-z- \.-]+$/', $f), 'bad character');
-    header('Content-Type: text/plain');
-    echo(date($f));
+    $fs =      $_REQUEST['format'];
+    $a = explode(',', $fs);
+    $ra = [];
+    foreach($a as $f) {    
+	kwas(!preg_match('/^[^A-Za-z- \.-]+$/', $f), 'bad character');
+	$ra[$f] = date($f);
+    }
+    
+    header('Content-Type: application/json');
+    echo(json_encode($ra));
 }
